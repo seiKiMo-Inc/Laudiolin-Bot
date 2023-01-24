7d4efc76cf2fbce2659b85f6996f6edb2aa4ec72
KingRainbow44 <kobedo11@gmail.com>
2023-01-24 01:39:03 -0500

    Match a query to audio sources

---
+++ src/main/java/moe/seikimo/laudiolin/objects/Source.java
@@ -0,0 +1,11 @@
+package moe.seikimo.laudiolin.objects;
+
+/**
+ * Audio sources.
+ */
+public enum Source {
+    YOUTUBE,
+    SPOTIFY,
+    LAUDIOLIN,
+    UNKNOWN
+}
--- src/main/java/moe/seikimo/laudiolin/utils/AudioUtil.java
+++ src/main/java/moe/seikimo/laudiolin/utils/AudioUtil.java
@@ -1,6 +1,7 @@
 package moe.seikimo.laudiolin.utils;
 
 import moe.seikimo.laudiolin.audio.LaudiolinAudioManager;
+import moe.seikimo.laudiolin.objects.Source;
 import tech.xigam.cch.utils.Interaction;
 
 /**
@@ -15,5 +16,24 @@ public interface AudioUtil {
         var url = interaction.getArgument("query",
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ", String.class);
         LaudiolinAudioManager.getInstance().play(url, interaction);
+    }
+
+    /**
+     * Identifies a query from a source.
+     * @param query The query.
+     * @return The source.
+     */
+    static Source identify(String query) {
+        if (query.contains("youtu.be") ||
+            query.contains("youtube.com"))
+            return Source.YOUTUBE;
+
+        if (query.contains("open.spotify.com"))
+            return Source.SPOTIFY;
+
+        if (query.contains("laudiolin.seikimo.moe"))
+            return Source.LAUDIOLIN;
+
+        return Source.UNKNOWN;
     }
 }
