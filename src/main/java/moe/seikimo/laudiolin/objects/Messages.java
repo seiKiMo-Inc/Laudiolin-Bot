7e7a29e33605f4a54c9b718cd3b7e088cd55bfda
KingRainbow44 <kobedo11@gmail.com>
2023-01-23 23:40:10 -0500

    Add collection of duplicate response messages

---
+++ src/main/java/moe/seikimo/laudiolin/objects/Messages.java
@@ -0,0 +1,21 @@
+package moe.seikimo.laudiolin.objects;
+
+import moe.seikimo.laudiolin.utils.MessageUtil;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+/**
+ * Pre-made messages.
+ */
+public interface Messages {
+    MessageEmbed USER_NOT_IN_VOICE = MessageUtil.generic(
+        "You are not in a voice channel.", MessageType.ERROR);
+    MessageEmbed BOT_NOT_IN_VOICE = MessageUtil.generic(
+        "I am not in a voice channel.", MessageType.ERROR);
+    MessageEmbed ALREADY_IN_VOICE = MessageUtil.generic(
+        "I am already in a voice channel.", MessageType.ERROR);
+
+    MessageEmbed NO_RESULTS_FOUND = MessageUtil.generic(
+        "Unable to find results for that query.", MessageType.ERROR);
+    MessageEmbed UNABLE_TO_PLAY = MessageUtil.generic(
+        "Unable to play the requested track.", MessageType.ERROR);
+}
